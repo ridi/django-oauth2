b9e95369b701e80b303015c7b7184183a6a92a99
김정근 <snorlax@ridi.com>
2018-03-08 15:59:56 +0900

    initialize project

---
+++ Makefile
@@ -0,0 +1,24 @@
+.PHONY:
+
+help:
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+lint:
+	@python3.6 $(shell which pylint) ./src/ --rcfile=.pylintrc && flake8
+
+test:
+	@python3.6 runtests.py
+
+
+# Release
+dist:
+	rm -rf ./dist
+	python setup.py sdist
+
+pypi-upload:
+	twine upload dist/*
+
+pypi-test-upload:
+    twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+release: dist pypi-upload
---
+++ runtests.py
@@ -0,0 +1,58 @@
+import os
+import sys
+
+import django
+from django.conf import settings
+
+sys.path.append(os.path.abspath('./src'))
+
+
+SETTINGS_DICT = {
+    'DEBUG': True,
+    'USE_TZ': True,
+    'DATABASES': {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:'
+        }
+    },
+    'AUTH_USER_MODEL': 'django_oauth2_client.RidiUser',
+    'INSTALLED_APPS': [
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sites',
+        'django_oauth2_client',
+        'oauth2_client',
+    ],
+    'MIDDLEWARE_CLASSES': (
+        'django_oauth2_client.middlewares.AuthenticationMiddleware',
+    ),
+    'RIDI_OAUTH2_JWT_SECRET': 'dummy_jwt_secret',
+    'RIDI_OAUTH2_CLIENT_ID': 'dummy_client_id',
+    'RIDI_OAUTH2_CLIENT_SECRET': 'dummy_client_secret',
+
+    'RIDI_OAUTH2_AUTHORIZATION_URL': 'http://localhost/oauth2/authorize/',
+    'RIDI_OAUTH2_TOKEN_URL': 'http://localhost/oauth2/token/',
+}
+
+
+def run_tests(*test_args):
+    if not test_args:
+        test_args = ['django_oauth2_client', 'oauth2_client']
+
+    settings.configure(**SETTINGS_DICT)
+
+    django.setup()
+
+    # Run tests
+    from django.test.utils import get_runner
+    TestRunner = get_runner(settings)
+
+    test_runner = TestRunner()
+    failures = test_runner.run_tests(test_args, interactive=False)
+
+    sys.exit(bool(failures))
+
+
+if __name__ == '__main__':
+    run_tests(*sys.argv[1:])
