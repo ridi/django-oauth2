118f4c1ca8cf06ddf402ca4f6aaf488bea096fa5
김정근 <snorlax@ridi.com>
2018-03-09 16:16:58 +0900

    test runner 수정

--- runtests.py
+++ runtests.py
@@ -16,20 +16,20 @@ SETTINGS_DICT = {
             'NAME': ':memory:'
         }
     },
-    'AUTH_USER_MODEL': 'django_oauth2_client.RidiUser',
+    'AUTH_USER_MODEL': 'ridi_django_oauth2_resource.RidiUser',
     'INSTALLED_APPS': [
         'django.contrib.auth',
         'django.contrib.contenttypes',
         'django.contrib.sites',
-        'django_oauth2_client',
+        'ridi_django_oauth2_resource',
         'tests',
     ],
     'MIDDLEWARE_CLASSES': (
-        'django_oauth2_client.middlewares.AuthenticationMiddleware',
+        'ridi_django_oauth2_resource.middlewares.AuthenticationMiddleware',
     ),
     'RIDI_OAUTH2_JWT_SECRET': 'dummy_jwt_secret',
-    'RIDI_OAUTH2_CLIENT_ID': 'dummy_client_id',
-    'RIDI_OAUTH2_CLIENT_SECRET': 'dummy_client_secret',
+    'RIDI_ridi_oauth2_ID': 'dummy_client_id',
+    'RIDI_ridi_oauth2_SECRET': 'dummy_client_secret',
 
     'RIDI_OAUTH2_AUTHORIZATION_URL': 'http://localhost/oauth2/authorize/',
     'RIDI_OAUTH2_TOKEN_URL': 'http://localhost/oauth2/token/',
