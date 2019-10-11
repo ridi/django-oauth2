fe79b8bae0408e8685b89095c866502899808515
seungmin paik <sm.paik@ridi.com>
2019-10-11 11:57:38 +0900

    version updated to 1.0.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-version = '1.0.1'
+version = '1.0.2'
 
 # When the project is installed by pip, this is the specification that is used to install its dependencies.
 install_requires = [
