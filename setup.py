1f0ff5de480d367b634040dbba63ff17c9a558ce
김정근 <snorlax@ridi.com>
2020-04-27 15:57:47 +0900

    버전 정보 변경

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-version = '1.0.7'
+version = '1.0.6'
 
 # When the project is installed by pip, this is the specification that is used to install its dependencies.
 install_requires = [
